fbc85b815d563113e12a2daf62d49713d4e55b6d
Mujahid12313019 <sagarmojahidhossen@gmail.com>
2026-06-14 19:34:50 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.post("/chunk", (req, res) => {
+  const text = req.body.text;
+
+  const size = 1000;
+  const overlap = 200;
+
+  let chunks = [];
+
+  for (let i = 0; i < text.length; i += (size - overlap)) {
+    chunks.push(text.slice(i, i + size));
+  }
+
+  res.json({ chunks });
+});
+
+app.listen(3000, () => console.log("Running"));
